06c6124a3619ef2198d0b0c19db233aaed1495aa
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2024-03-03 17:16:48 -0500

    openbox themes part 2

---
+++ .config/openbox/themes/evangelion/theme.bash
@@ -0,0 +1,92 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2022 Aditya Shakya <adi1090x@gmail.com>
+#
+# Everforest Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#211a27'
+foreground='#64c67d'
+color0='#a77eeb'
+color1='#a77eeb'
+color2='#7cc4a0'
+color3='#a77eeb'
+color4='#8b61e3'
+color5='#a77eeb'
+color6='#8b61e3'
+color7='#a77eeb'
+color8='#8b61e3'
+color9='#a77eeb'
+color10='#8b61e3'
+color11='#a77eeb'
+color12='#8b61e3'
+color13='#a77eeb'
+color14='#8b61e3'
+color15='#a77eeb'
+
+accent='#a77eeb'
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='JetBrains Mono:size=10;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Zafiro'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='manhattan.conf'
+geany_font='JetBrains Mono 10'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Manhattan'
+icon_theme='oomox-aesthetic-dark'
+cursor_theme='elementary'
+
+# Openbox
+ob_theme='Dracula'
+ob_layout='NLIMC'
+ob_font='JetBrains Mono'
+ob_font_size='9'
+ob_menu='menu-icons.xml'
+ob_margin_t='0'
+ob_margin_b='0'
+ob_margin_l='0'
+ob_margin_r='0'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='20x78'
+dunst_origin='top-right'
+dunst_font='JetBrains Mono 10'
+dunst_border='0'
+dunst_separator='2'
+
+# Plank
+plank_hmode='intelligent'
+plank_offset='0'
+plank_position='bottom'
+plank_theme='Transparent'
+plank_icon_size='32'
+plank_zoom_percent='120'
+
+# Picom
+picom_backend='glx'
+picom_corner='12'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='none'
+picom_blur_strength='0'
