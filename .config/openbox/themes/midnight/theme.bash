f4c11e6327411bd9a14f98e63e90b0ba342ce4ee
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2024-03-03 17:18:44 -0500

    openbox themes (part 3)

---
+++ .config/openbox/themes/midnight/theme.bash
@@ -0,0 +1,96 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2024 Aditya Shakya <adi1090x@gmail.com>
+#
+# Manhattan Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#1b1424'
+foreground='#898D90'
+color0='#20262c' #todo
+color1='#db86ba'
+color2='#74dd91' #todo (player and bluetooth)
+color3='#e49186' #todo (speaker)
+color4='#75dbe1'
+color5='#b4a1db'
+color6='#9ee9ea'
+color7='#f1fcf9'
+color8='#465463'
+color9='#d04e9d'
+color10='#4bc66d'
+color11='#db695b'
+color12='#3dbac2'
+color13='#825ece'
+color14='#62cdcd'
+color15='#e0e5e5'
+color16='#b4a1db'
+
+element_bg='#1b1424' #undo? (1F252B)
+element_fg='#898D90' #undo? (F0FBF8)
+element_hl='#bbb5b4' #undo? (B4A1DB)
+
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="$wdir/wallpaper"
+
+# Polybar
+polybar_font='UbuntuMono Nerd Font:size=10;3'
+
+# Rofi
+rofi_font='UbuntuMono Nerd Font 10'
+rofi_icon='Nordic-Folders'
+
+# Terminal
+terminal_font_name='UbuntuMono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='manhattan.conf'
+geany_font='UbuntuMono Nerd Font 10'
+
+# Appearance
+gtk_font='UbuntuMono Nerd Font 10'
+gtk_theme='Manhattan'
+icon_theme='Zafiro-Purple-Dark'
+cursor_theme='elementary'
+
+# Openbox
+ob_theme='Manhattan'
+ob_layout='LIMC'
+ob_font='UbuntuMono Nerd Font'
+ob_font_size='10'
+ob_menu='menu-icons.xml'
+ob_margin_t='10'
+ob_margin_b='48'
+ob_margin_l='10'
+ob_margin_r='10'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='20x20'
+dunst_origin='top-right'
+dunst_font='UbuntuMono Nerd Font 9'
+dunst_border='0'
+dunst_separator='2'
+
+# Plank
+plank_hmode='auto'
+plank_offset='80'
+plank_position='right'
+plank_theme='Transparent'
+plank_icon_size='32'
+plank_zoom_percent='120'
+
+# Picom
+picom_backend='glx'
+picom_corner='6'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='none'
+picom_blur_strength='0'
